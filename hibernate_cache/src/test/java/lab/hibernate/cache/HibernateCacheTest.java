4de7ec0b1a4474e645ca8200513132a70a2bf9aa
franklu918 <franklu918@gmail.com>
2013-12-19 12:57:27 +0800

    去掉多余引用

--- hibernate_cache/src/test/java/lab/hibernate/cache/HibernateCacheTest.java
+++ hibernate_cache/src/test/java/lab/hibernate/cache/HibernateCacheTest.java
@@ -6,11 +6,9 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
-import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
